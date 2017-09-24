32ccacf3d070884e3c2dd917ab92b4467890851a
Elias Keis <elKei24@users.noreply.github.com>
2017-09-24 22:45:23 +0200

    added CellPanel and CellListener

---
+++ src/gui/src/CellPanel.kt
@@ -0,0 +1,26 @@
+import java.awt.Color
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.JPanel
+
+class CellPanel(val cell: Cell): JPanel() {
+    init {
+        updateCellState()
+
+        addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(p0: MouseEvent?) {
+                cell.living = !cell.living
+            }
+        })
+
+        cell.addListener(object : CellListener {
+            override fun cellLivingChanged(cell: Cell) {
+                updateCellState()
+            }
+        })
+    }
+
+    private fun updateCellState() {
+        background = if (cell.living) Color.BLACK else Color.WHITE
+    }
+}
--- src/model/src/Cell.kt
+++ src/model/src/Cell.kt
@@ -1,10 +1,27 @@
+import java.util.concurrent.CopyOnWriteArrayList
+
 class Cell(var living : Boolean = false) {
+    private val listeners = CopyOnWriteArrayList<CellListener>()
+
     fun updateToNextGeneration(livingNeighbours: Int) {
-        living = if (!living)
+        val newLiving = getNextGenerationLiving(livingNeighbours)
+        if (living != newLiving) {
+            living = newLiving
+            notifyCellLivingChanged()
+        }
+    }
+    private fun getNextGenerationLiving(livingNeighbours: Int) : Boolean {
+        return if (!living)
             livingNeighbours == 3
         else
             livingNeighbours == 2 || livingNeighbours == 3
     }
 
+    fun addListener(cellListener: CellListener) = listeners.add(cellListener)
+    fun removeListener(cellListener: CellListener) = listeners.remove(cellListener)
+    private fun notifyCellLivingChanged() = listeners.forEach{it.cellLivingChanged(this)}
+}
 
+interface CellListener {
+    fun cellLivingChanged(cell: Cell)
 }
