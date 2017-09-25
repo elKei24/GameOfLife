b178fc2c27a987fd87c4c389876b1a3be495457e
Elias Keis <elKei24@users.noreply.github.com>
2017-09-25 16:31:59 +0200

    use separate main function that sets look and feel to Nimbus

---
+++ src/gui/src/com/elkei/gol/gui/Main.kt
@@ -0,0 +1,22 @@
+package com.elkei.gol.gui
+
+import javax.swing.UIManager
+
+
+fun main(args: Array<String>) {
+    setNimbusLookAndFeel()
+    MainFrame().isVisible = true
+}
+
+private fun setNimbusLookAndFeel() {
+    try {
+        for (info in UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus" == info.name) {
+                UIManager.setLookAndFeel(info.className)
+                break
+            }
+        }
+    } catch (e: Exception) {
+        //well, then just use the default look and feel
+    }
+}
