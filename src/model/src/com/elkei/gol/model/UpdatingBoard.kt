b0d473cb6765f5847a025b07ef18f2f4329ffc72
Elias Keis <elKei24@users.noreply.github.com>
2017-09-26 12:15:59 +0200

    added KDoc for UpdatingBoard

--- src/model/src/com/elkei/gol/model/UpdatingBoard.kt
+++ src/model/src/com/elkei/gol/model/UpdatingBoard.kt
@@ -4,6 +4,16 @@ import com.elkei.gol.model.util.ListenersManager
 import com.elkei.gol.model.util.Observable
 import java.util.*
 
+/**
+ * This class describes a Game-Of-Live board that can do generation updates by itself regularly.
+ *
+ * @see Board
+ *
+ * @param size Size of the constructed board.
+ * @param init This function is called on each initialization of a [Cell] of the board, passing the coordinate of the new
+ *              cell. If true is returned, then [Cell.living] will be set to true at the beginning, to false otherwise.
+ * @param msBetweenUpdates Initial value of [UpdatingBoard.msBetweenUpdates]
+ */
 class UpdatingBoard(
         size: Coordinate,
         init: (Coordinate) -> Boolean = {false},
@@ -11,6 +21,12 @@ class UpdatingBoard(
 ) : Board(size, init), Observable<UpdatingBoardListener> {
     private val listenersManager = ListenersManager<UpdatingBoardListener>()
     private var timer: Timer? = null
+
+    /**
+     * The generation counter is increased whenever this board proceeds to the next generation.
+     *
+     * @see UpdatingBoard.resetGenerationCounter
+     */
     var generationCounter = 0
     private set(value) {
         if (value != field) {
@@ -19,6 +35,10 @@ class UpdatingBoard(
         }
     }
 
+    /**
+     * This property describes if there is a background thread running to update the board to the next generation every
+     * [msBetweenUpdates] milliseconds.
+     */
     var running = false
     private set(value) {
         if (field != value) {
@@ -27,6 +47,10 @@ class UpdatingBoard(
         }
     }
 
+    /**
+     * This property describes how long to wait between two generation updates executed by the background thread.
+     * The time needed for updating the board itself will be subtracted from this value.
+     */
     var msBetweenUpdates: Long = msBetweenUpdates
     set(value) {
         val oldValue = field
@@ -47,6 +71,12 @@ class UpdatingBoard(
         running = true
     }
 
+    private inner class BoardUpdateTask : TimerTask() {
+        override fun run() {
+            updateToNextGeneration()
+        }
+    }
+
     /**
      * Stops updating the board in the background. No further updates to the board will be made.
      */
@@ -56,25 +86,49 @@ class UpdatingBoard(
         running = false
     }
 
+    /**
+     * Updates every cell to the next generation, calling [Cell.updateToNextGeneration].
+     * After the generation update, [generationCounter] is increased by one.
+     */
     override fun updateToNextGeneration() {
         super.updateToNextGeneration()
         generationCounter++
     }
 
+    /**
+     * Resets [generationCounter] to zero.
+     */
     fun resetGenerationCounter() {
         generationCounter = 0
     }
 
-    private inner class BoardUpdateTask : TimerTask() {
-        override fun run() {
-            updateToNextGeneration()
-        }
-    }
-
+    /**
+     * Calls [UpdatingBoardListener.updatingBoardRunningChanged] for each listener
+     */
     private fun notifyRunningChanged() = listenersManager.forEach { it.updatingBoardRunningChanged(this, running) }
+
+    /**
+     * Calls [UpdatingBoardListener.updatingBoardSpeedChanged] for each listener
+     */
     private fun notifySpeedChanged() = listenersManager.forEach { it.updatingBoardSpeedChanged(this, msBetweenUpdates) }
+
+    /**
+     * Calls [UpdatingBoardListener.updatingBoardGenerationChanged] for each listener
+     */
     private fun notifyGenerationChanged() = listenersManager.forEach { it.updatingBoardGenerationChanged(this, generationCounter) }
+
+    /**
+     * Adds an [UpdatingBoardListener]. It will be notified when special events occur in this [UpdatingBoard]
+     *
+     * @param listener the listener that will be notified on events
+     */
     override fun addListener(listener: UpdatingBoardListener) = listenersManager.addListener(listener)
+
+    /**
+     * Removes the given listener. It will no longer be notified.
+     *
+     * @param listener the listener to remove
+     */
     override fun removeListener(listener: UpdatingBoardListener) = listenersManager.removeListener(listener)
 }
 
