42e9865209f5f88c4ce6d7b519a6fc7e862586eb
Elias Keis <elKei24@users.noreply.github.com>
2017-09-27 00:11:57 +0200

    Let UpdatingBoardPanel do the mouse stuff
    If each CellPanel is its own listener, every cell state is inverted.
    Now, only cell states of CellPanels are inverted if they have the same
    state as the one where the dragging started.

--- src/gui/src/com/elkei/gol/gui/modelpanels/CellPanel.kt
+++ src/gui/src/com/elkei/gol/gui/modelpanels/CellPanel.kt
@@ -4,43 +4,27 @@ import com.elkei.gol.model.Cell
 import com.elkei.gol.model.CellListener
 import com.elkei.gol.model.util.ListenersManager
 import java.awt.Color
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
 import javax.swing.JPanel
-import javax.swing.SwingUtilities
 
 class CellPanel(val cell: Cell): JPanel() {
-    private var mouseClicking = false
-    set(value) {
-        if (value != field) {
-            field = value
-            if (!value) manipulateCell()
-            updateCellState()
-        }
-    }
-
     private val listeners = ListenersManager<CellManipulationListener>()
+
+    private var mouseClicking = false
+    private set(value) {
+        if (field != value) {
+            field = value
+            if (livingAfterMouseClick != cell.living) manipulateCell()
+        }
+        updateCellState()
+    }
+    private var livingAfterMouseClick = false
+    private set(value) {
+        field = value
+        updateCellState()
+    }
 
     init {
         updateCellState()
-
-        addMouseListener(object : MouseAdapter() {
-            override fun mousePressed(p0: MouseEvent?) {
-                mouseClicking = true
-            }
-
-            override fun mouseReleased(p0: MouseEvent?) {
-                mouseClicking = false
-            }
-
-            override fun mouseEntered(p0: MouseEvent?) {
-                mouseClicking = (SwingUtilities.isLeftMouseButton(p0))
-            }
-
-            override fun mouseExited(p0: MouseEvent?) {
-                mouseClicking = false
-            }
-        })
 
         cell.addListener(object : CellListener {
             override fun cellLivingChanged(cell: Cell) {
@@ -49,20 +33,36 @@ class CellPanel(val cell: Cell): JPanel() {
         })
     }
 
+    fun mousePressed(livingAfterMouseClick: Boolean) {
+        mouseClicking = true
+        this.livingAfterMouseClick = livingAfterMouseClick
+    }
+
+    fun mouseReleased() {
+        mouseClicking = false
+    }
+
     private fun manipulateCell() {
         cell.living = !cell.living
         notifyManipulation()
     }
 
     private fun updateCellState() {
-        background = when {
-            cell.living && !mouseClicking -> Color.BLACK
-            cell.living && mouseClicking -> Color.DARK_GRAY
-            !cell.living && mouseClicking -> Color.LIGHT_GRAY
-            else -> Color.WHITE
+        background = if (cell.living) {
+            if (!checkIfLivingChangesSoon())
+                Color.BLACK
+            else
+                Color.DARK_GRAY
+        } else {
+            if (!checkIfLivingChangesSoon()) {
+                Color.WHITE
+            } else
+                Color.LIGHT_GRAY
         }
         repaint()
     }
+
+    private fun checkIfLivingChangesSoon() = mouseClicking && (cell.living != livingAfterMouseClick)
 
     fun addListener(listener: CellManipulationListener) = listeners.addListener(listener)
     fun removeListener(listener: CellManipulationListener) = listeners.removeListener(listener)
--- src/gui/src/com/elkei/gol/gui/modelpanels/UpdatingBoardPanel.kt
+++ src/gui/src/com/elkei/gol/gui/modelpanels/UpdatingBoardPanel.kt
@@ -6,6 +6,8 @@ import com.elkei.gol.model.UpdatingBoardListener
 import com.elkei.gol.model.util.Observable
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import javax.swing.JPanel
 
 class UpdatingBoardPanel(var board: UpdatingBoard) :
@@ -17,9 +19,11 @@ class UpdatingBoardPanel(var board: UpdatingBoard) :
                 board.resetGenerationCounter()
             }
         }
+        val cellMouseListener = CellMouseListener()
         board.allCoordinates().forEach { coordinate ->
             val newCellPanel = addNewCellPanelAt(coordinate)
             newCellPanel.addListener(cellPanelListener)
+            newCellPanel.addMouseListener(cellMouseListener)
         }
     }
 
@@ -36,4 +40,45 @@ class UpdatingBoardPanel(var board: UpdatingBoard) :
     }
 
     private fun getNewCellPanelForCoordinate(coordinate: Coordinate) = CellPanel(board.getItemForCoordinate(coordinate))
+
+    private class CellMouseListener : MouseAdapter() {
+        var mouseClicking = false
+        var newLiving = true
+        var currentMouseComponent: CellPanel? = null //workaround for mouseRelease called by component where mouse was pressed
+
+        override fun mousePressed(event: MouseEvent) {
+            val sender = event.component
+            if (sender is CellPanel) {
+                currentMouseComponent = sender
+                mouseClicking = true
+                newLiving = !sender.cell.living
+                sender.mousePressed(newLiving)
+            }
+        }
+
+        override fun mouseReleased(event: MouseEvent) {
+            val sender = event.component
+            if (sender is CellPanel && mouseClicking) {
+                currentMouseComponent?.mouseReleased()
+                mouseClicking = false
+            }
+        }
+
+        override fun mouseEntered(event: MouseEvent) {
+            val sender = event.component
+            if (sender is CellPanel && mouseClicking) {
+                currentMouseComponent = sender
+                sender.mousePressed(newLiving)
+            }
+        }
+
+        override fun mouseExited(event: MouseEvent) {
+            val sender = event.component
+            if (sender is CellPanel && mouseClicking) {
+                sender.mouseReleased()
+            }
+        }
+
+
+    }
 }
